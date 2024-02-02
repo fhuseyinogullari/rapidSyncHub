5f8bedd65fec7e1bbae54e575b260ca19ef9ee61
fhuseyinogullari <fatih.hsu1907@gmail.com>
2024-02-02 19:58:15 +0300

    initial commit

---
+++ src/main/java/com/github/fhuseyinogullari/rapidsynchub/RapidSyncHubApplication.java
@@ -0,0 +1,13 @@
+package com.github.fhuseyinogullari.rapidsynchub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapidSyncHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapidSyncHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/fhuseyinogullari/rapidsynchub/RapidSyncHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fhuseyinogullari.rapidsynchub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapidSyncHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
